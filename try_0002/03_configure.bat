e5dd8ce4b0a16b2057a0eeae4e089a6b01ad4400
mariuszekpl <max@olo.com.pl>
2014-01-30 21:10:18 +0100

    next step

--- try_0002/03_configure.bat
+++
@@ -1,7 +0,0 @@
-set PATH=C:\MinGW\bin;D:\Qt\gcc-linaro-arm-linux-gnueabihf-2012.09-20120921_win32\bin;%PATH%
-
-cd "..\qtsrc\qtbase" 
-
-configure.bat -platform win32-g++ -xplatform linux-arm-gnueabi-g++   -no-opengl -no-neon  -nomake tests -opensource -confirm-license   
-
-pause
